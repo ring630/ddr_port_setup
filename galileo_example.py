96681ccdf4e615288025315d5be4f14ac13e7fa0
hzhou <ring630@gmail.com>
2022-07-26 13:37:58 +0200

    update

--- galileo_example.py
+++ galileo_example.py
@@ -1,34 +1,11 @@
-import os
-import shutil
-import tempfile
-from pyaedt import generate_unique_name, examples
 from utils.ddr_port_setup import DDRPortSetup
+from utils.galileo_example import get_galileo_exmaple_board
 
-
-###############################################################################
-# Download file
-# ~~~~~~~~~~~~~
-# Download the AEDB file and copy it in the temporary folder.
-
-
-tmpfold = tempfile.gettempdir()
-temp_folder = os.path.join(tmpfold, generate_unique_name("Example"))
-if not os.path.exists(temp_folder):
-    os.makedirs(temp_folder)
-example_path = examples.download_aedb()
-targetfolder = os.path.join(temp_folder, "Galileo.aedb")
-if os.path.exists(targetfolder):
-    shutil.rmtree(targetfolder)
-shutil.copytree(example_path[:-8], targetfolder)
-targetfile = os.path.join(targetfolder)
-
-print(targetfile)
-
-###############################################################################
+targetfile = get_galileo_exmaple_board()
 
 app = DDRPortSetup(edb_fpath=targetfile,
-                       controller_refdes="U2A5",
-                       dram_refdes=["U1A1", "U1B5"],
-                       dram_type="ddr3_x8",
-                       save_edb_as_fname=r"result_project\Galileo_new.aedb"
-                       )
+                   controller_refdes="U2A5",
+                   dram_refdes=["U1A1", "U1B5"],
+                   dram_type="ddr3_x8",
+                   save_edb_as_fname=r"result_project\Galileo_new.aedb"
+                   )
